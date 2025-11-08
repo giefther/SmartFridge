5e835b1aa613b987efbf9d021572134330856b70
giefther <giefther@outlook.com>
2025-11-09 01:24:14 +0300

    Implement PasswordHasher with SHA256 hashing

--- SmartFridge.Core/Utils/PasswordHasher.cs
+++ SmartFridge.Core/Utils/PasswordHasher.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartFridge.Core.Utils
 {
-    internal class PasswordHasher
+    public static class PasswordHasher
     {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashedInput = HashPassword(password);
+            return hashedInput == hashedPassword;
+        }
     }
 }
